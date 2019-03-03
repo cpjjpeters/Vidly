af8fe26a16cab4e1a78289b7064b2d56283ab759
Carl Peters <cpjjpeters@msn.com>
2019-03-03 20:52:13 +0100

    recreate database etc

--- Vidly/Migrations/201903031948116_InitialOne.Designer.cs
+++ Vidly/Migrations/201903031948116_InitialOne.Designer.cs
@@ -6,14 +6,14 @@ namespace Vidly.Migrations
     using System.Data.Entity.Migrations.Infrastructure;
     using System.Resources;
     
-    [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class PopulateMembershipType : IMigrationMetadata
+    [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
+    public sealed partial class InitialOne : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(PopulateMembershipType));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialOne));
         
         string IMigrationMetadata.Id
         {
-            get { return "201812261748529_PopulateMembershipType"; }
+            get { return "201903031948116_InitialOne"; }
         }
         
         string IMigrationMetadata.Source
